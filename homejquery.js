8989c70238d19877c35dbb413513dfc5aa11dd0b
newtdogg <edwardfrancisnewton@live.com>
2018-02-15 23:32:50 +0000

    updated projects section

--- homejquery.js
+++ homejquery.js
@@ -6,12 +6,25 @@ sectionColours = {
               "Contact": "#C591B8"
                   }
 
+sectionPictures = {
+        "About": "me.png",
+   "Projects": "helmet1.png",
+      "Blog": "book.png",
+     "Fourth": "helmet2.png",
+    "Contact": "phone.png"
+  }
+
 sectionText = {
               "About": "Currently studying at Makers Academy, Europe's leading webdev bootcamp. Fascinated by the wide world of web development, from front end to back end.",
-         "Projects": "Here is a list of things from my github look how cool they are",
-            "Blog": "#BAC7BE",
+         "Projects": "<div><h4><a href='https://github.com/newtdogg/poker-bot'>Big Brain</a> </h4> <div> \
+         <div><p> A bot that can play heads up poker. Designed, tested and written from scratch</p></div> \
+         <div><h4><a href='https://github.com/newtdogg/Makers-BnB'>EZsleep</a> </h4> <div> \
+         <div><p> A website designed to emulate airBnB, front end and back end written in JavaScript, using nodeJs, expressJS, tested using mocha</p></div> ",
+            "Blog": "<div>My Blog is currently under construction, however here is a blog I wrote during the two weeks of the final project at Makers:</div><p class='contactBody'><a href='https://pokerprogressioninjava.wordpress.com/'>Poker Progression in Java</a></p>",
            "Fourth": "#548687",
-          "Contact": "#FFCE79"
+          "Contact": "<div><p class='contactTitle'>EMAIL:</p> <p class='contactBody'>edwardfrancisnewton@live.com</p></div> \
+          <div><p class='contactTitle'>GITHUB:</p> <p class='contactBody'><a href='https://github.com/newtdogg/'>github.com/newtdogg</a></p></div>\
+          <div><p class='contactTitle'>LINKEDIN:</p> <p class='contactBody'><a href='https://www.linkedin.com/in/edwardfrancisnewton/'>www.linkedin.com/in/edwardfrancisnewton</a></p></div>"
 }
 
 function displayNav(){
@@ -42,6 +55,7 @@ function displaySection(box){
   } else {
     $(`#header-box`).html(`${box}`)
   }
+  changePicture(box)
   sectionInnerText(box)
   removeHover(box)
   // changeSectionClick(id)
@@ -76,6 +90,7 @@ function expandNavSections(box){
     height: "20%",
     opacity: 1,
   })
+  $(`#images`).empty()
   $(`#${box}`).css({
     "border-top-width" : "0.6vmin",
               "padding": "0vmin"
@@ -103,4 +118,22 @@ function removeHover(box){
 function sectionInnerText(box){
   $(`#${box}-text`).html(sectionText[`${box}`])
   $(`#${box}-text`).animate({opacity: 1})
+}
+
+function changePicture(box){
+  if (box === "About"){
+    $("#images").html(`<img src="./images/${sectionPictures[box]}"></div>`)
+    $("#images").children('img').css({"bottom": "30%",
+                      "left": "20%"})
+  } else if (box === "Contact") {
+    $("#images").html(`<img src="./images/${sectionPictures[box]}"></div>`)
+    $("#images").children('img').css({bottom: "20%",
+                        left: "25%",
+                        "max-width": "60%"})
+  } else {
+    $("#images").html(`<img src="./images/${sectionPictures[box]}"></div>`)
+    $("#images").children('img').css({bottom: "30%",
+                      left: "20%",
+                  })
+  }
 }
